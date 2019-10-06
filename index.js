8401ebf826475404e90fd5e0c3b1fa58c85d7485
Kevin Sage <ksagedeveloper@gmail.com>
2019-10-06 02:07:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, firstName) {
   katzDeliLine.push(firstName);
-  return "Welcome, " + firstName + ", you are" + katzDeliLine.length + "in line." 
+  return "Welcome, " + firstName + ", you are number " + katzDeliLine.length + "in line." 
 }
