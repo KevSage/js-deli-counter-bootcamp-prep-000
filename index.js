c15ee20a626c90b9a849c263bc3affd82b011b1b
Kevin Sage <ksagedeveloper@gmail.com>
2019-10-06 02:07:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-function takeANumber(firstName, katzDeli.length) {
-katzDeli.push(firstName);
-console.log(katzDeliLine);
-console.log('Welcome, ' + firstName + ". You are now number " + katzDeli.length + " in line.");
-}
-
-function nowServing(){
-console.log('Now serving ' + katzDeli[0]);
-katzDeli.shift();
-if (katzDeli.length === 0) {
-console.log("There is nobody waiting to be served!");
-} else {
-console.log(katzDeli);
- }
-}
-
-function currentLine (katzDeliLine) {
-  var arr =[];
-  for (let i = 0; i < katzDeliLine.length; i++) 
-  return ("The current line is " + )
+function takeANumber(katzDeliLine, firstName) {
+  katzDeliLine.push(firstName);
+  return "Welcome, " + firstName + ", you are" + katzDeliLine.length + "in line." 
 }
