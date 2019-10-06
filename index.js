b91a858686f753b0dac9320c512b14e2e2d46d45
Kevin Sage <ksagedeveloper@gmail.com>
2019-10-06 01:41:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(firstName, katzDeli.length) {
+katzDeli.push(firstName);
+console.log(katzDeliLine);
+console.log('Welcome, ' + firstName + ". You are now number " + katzDeli.length + " in line.");
+}
+
+function nowServing(){
+console.log('Now serving ' + katzDeli[0]);
+katzDeli.shift();
+if (katzDeli.length === 0) {
+console.log("There is nobody waiting to be served!");
+} else {
+console.log(katzDeli);
+ }
+}
