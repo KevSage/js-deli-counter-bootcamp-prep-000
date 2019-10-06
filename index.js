2565d24430026a8e56b1614b280b08d5a8fc6336
Kevin Sage <ksagedeveloper@gmail.com>
2019-10-06 02:23:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function nowServing() {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return katzDeliLine[0];
     katzDeliLine.shift();
   }
 }
